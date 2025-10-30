ca078682f73e00bd5cce94bca6ef8be622e45da4
Bruno <b-d.dupuis@laposte.net>
2025-10-30 21:32:26 +0100

    first commit

---
+++ src/components/Footer.vue
@@ -0,0 +1,7 @@
+<template>
+  <footer>
+    <div>
+      <p>Made by <a href="@">Le Reacteur</a></p>
+    </div>
+  </footer>
+</template>
---
+++ src/components/Header.vue
@@ -0,0 +1,24 @@
+<template>
+  <header>
+    <div>
+      <p>Essayez d'abord, payez après</p>
+      <p>LIVRAISON & RETOUR GRATUITS *</p>
+      <p>Retour et remboursements</p>
+    </div>
+    <div>
+      <nav>
+        <a href="#">Femme</a>
+        <a href="#">Homme</a>
+        <a href="#">Enfant</a>
+      </nav>
+      <img src="../assets/img/logo_zalendo.svg" alt="logo-zalendo" />
+      <nav>
+        <p>icone 1</p>
+        <p>icone 2</p>
+        <p>icone 3</p>
+      </nav>
+    </div>
+  </header>
+</template>
+
+<style scoped></style>
